83979e1355100dfd49b1ccb40478fad8236dee3c
aurus.official <russelquinto.official@gmaill.com>
2025-11-13 18:46:40 +0800

    Changes compose.yaml and process.sh for js (frontend)

--- process.sh
+++ process.sh
@@ -3,7 +3,10 @@
 cd ~/Programming/gadget-rental/backend
 mvn clean
 mvn verify
-
 docker build . -t rental-server
-cd ..
+
+cd ~/Programming/gadget-rental/frontend/
+docker build . -t rental-website
+
+cd ~/Programming/gadget-rental/
 docker-compose up
